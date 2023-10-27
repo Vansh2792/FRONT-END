aea95222adca726e3fd94d6f6ea982eebc86890e
Vansh2792 <129400802+Vansh2792@users.noreply.github.com>
2023-10-27 11:08:33 +0100

    Add files via upload

---
+++ aboutus.js
@@ -0,0 +1,34 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+
+
+
+
+
+
+var sidebar = document.getElementById("sidebar");
+
+function openmenu(){
+    sidebar.style.right = "0";
+}
+
+function closemenu(){
+    sidebar.style.right = "-200px";
+}
+
+
+
